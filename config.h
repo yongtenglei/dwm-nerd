3223f5aa521c820d630a24a8c0670142503b113c
rey <3065588496@qq.com>
2021-07-03 10:56:22 +0800

    ADD gridmode and change README.md

--- config.h
+++ config.h
@@ -73,11 +73,13 @@ static const int nmaster = 1;   /* number of clients in master area */
 static const int resizehints =
     1; /* 1 means respect size hints in tiled resizals */
 
+#include "layouts.c"
 static const Layout layouts[] = {
     /* symbol     arrange function */
     {"[ ]", tile}, /* first entry is default */
     {"><>", NULL}, /* no layout function means floating behavior */
     {"[M]", monocle},
+    {"HHH", grid},
 };
 
 /* key definitions */
@@ -152,6 +154,7 @@ static Key keys[] = {
     {MODKEY, XK_t, setlayout, {.v = &layouts[0]}},
     {MODKEY, XK_f, setlayout, {.v = &layouts[1]}},
     {MODKEY, XK_m, setlayout, {.v = &layouts[2]}},
+    {MODKEY, XK_g, setlayout, {.v = &layouts[3]}},
     {MODKEY, XK_space, setlayout, {0}},
     {MODKEY | ShiftMask, XK_f, fullscreen, {0}},
     {MODKEY | ShiftMask, XK_space, togglefloating, {0}},
